99da67024522e19fc95e8857e3bb81035bbba4b6
golangframework <server.ozga@gmail.com>
2026-01-14 13:12:20 +0100

    Convert modyfication. Now with error.

--- Convert.go
+++ Convert.go
@@ -8,19 +8,26 @@ package slices
 //
 // The source sequence that contains the elements TObject to be converted to type TResult.
 //
-// 	convert convert[TObject, TResult]
+// 	convert func(object TObject) (TResult, error)
 //
-// A function that converts element of type TObject to another element of type TResult.
+// A function that converts element of type TObject to another element of type TResult, and returns error while converting.
 //
 // # Returns
 //
 //	result []TResult
 //
 // A sequence that contains each element of the source sequence converted to the specified type.
-func Convert[TSource ~[]TObject, TObject any, TResult any](source TSource, convert convert[TObject, TResult]) (result []TResult) {
+//
+//	err error
+//
+// An error if convertion fails for at lest one object.
+func Convert[TSource ~[]TObject, TObject any, TResult any](source TSource, convert func(object TObject) (TResult, error)) (result []TResult, err error) {
 	result = make([]TResult, len(source))
 	for i := range source {
-		result[i] = convert(source[i])
+		result[i], err = convert(source[i])
+		if err != nil {
+			return nil, err
+		}
 	}
-	return result
+	return result, nil
 }
--- Slices.go
+++ Slices.go
@@ -3,5 +3,5 @@ package slices
 type valueSelector[TObject any, TResult any] = func(object TObject) TResult
 type accumulate[TAccumulator any, TObject any] = func(accumulator TAccumulator, object TObject) TAccumulator
 type predicate[TObject any] = func(object TObject) bool
-type convert[TObject any, TResult any] = func(object TObject) TResult
+type convert[TObject any, TResult any] = func(object TObject) (TResult, error)
 type keySelector[TObject any, TKey comparable] = func(object TObject) TKey
